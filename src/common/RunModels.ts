f30e400d920ebec8b2549cf7bd497389827a39c0
Leeffkk <yufanwang3@gmail.com>
2022-12-23 08:26:11 +0800

    more logs in runLead

--- src/common/RunModels.ts
+++ src/common/RunModels.ts
@@ -15,19 +15,19 @@ export class RunModels {
             const pyprog = spawn('python', [config_to_use.leadCode, inputFile, outputFile, img_mod], {
                 cwd: config_to_use.leadCodeDir
             })
-            console.log('lead run model started')
+            console.log('lead run model started:' + '\n\t' + config_to_use.leadCode + '\n\t' + inputFile + '\n\t' + outputFile + '\n\t' + img_mod);
             pyprog.stdout.on('data', function(data:any){
-                console.log(data.toString());
+                console.log('child process data ' + data.toString());
                 // resolve(data);
             })
             pyprog.on('close', (code:any) => {
-                console.log('child process close all stdio with code ${code}');
+                console.log('child process close all stdio with code: ' + code.toString());
             });
             pyprog.on('exit', (code:any) => {
-                console.log('child process exit with code ${code}');
+                console.log('child process exit with code ' + code.toString());
             });
             pyprog.on('error', (code:any) => {
-                console.log('child process error with code ${code}');
+                console.log('child process error with code ' + code.toString());
             });
             
             // pyprog.stdout.on('data', (data:any) => {
