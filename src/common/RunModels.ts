78489732ba6db656eec5be23093ccf794255aafb
Leeffkk <yufanwang3@gmail.com>
2023-01-25 20:56:30 +0800

    runMotion() finished

--- src/common/RunModels.ts
+++ src/common/RunModels.ts
@@ -12,7 +12,7 @@ export class RunModels {
     runLead(inputFile:string, outputFile:string, img_mod:string): Promise<boolean>{
         return new Promise(function(resolve, reject){
             const spawn = require("child_process").spawn;
-            const pyprog = spawn('python', [config_to_use.leadCode, inputFile, outputFile, img_mod], {
+            const pyprog = spawn(config_to_use.leadPythonDir, [config_to_use.leadCode, inputFile, outputFile, img_mod], {
                 cwd: config_to_use.leadCodeDir
             })
             console.log('lead run model started:' + '\n\t' + config_to_use.leadCode + '\n\t' + inputFile + '\n\t' + outputFile + '\n\t' + img_mod);
@@ -34,6 +34,34 @@ export class RunModels {
             //     reject(data);
             // })
             console.log('lead run model finished')
+        })
+    }
+
+    runMotion(inputFile1:string, inputFile2:string, outputFile:string, img_mod:string): Promise<boolean>{
+        return new Promise(function(resolve, reject){
+            const spawn = require("child_process").spawn;
+            const pyprog = spawn(config_to_use.motionPythonDir, [config_to_use.motionCode, inputFile1, inputFile2, outputFile], {
+                cwd: config_to_use.motionCodeDir
+            })
+            console.log('motion run model started:' + '\n\t' + config_to_use.motionCode + '\n\t' + inputFile1 + '\n\t' + inputFile2 + '\n\t' + outputFile);
+            pyprog.stdout.on('data', function(data:any){
+                console.log('child process data ' + data.toString());
+                // resolve(data);
+            })
+            pyprog.on('close', (code:any) => {
+                console.log('child process close all stdio with code: ' + code.toString());
+            });
+            pyprog.on('exit', (code:any) => {
+                console.log('child process exit with code ' + code.toString());
+            });
+            pyprog.on('error', (code:any) => {
+                console.log('child process error with code ' + code.toString());
+            });
+            
+            // pyprog.stdout.on('data', (data:any) => {
+            //     reject(data);
+            // })
+            console.log('motion run model finished')
         })
     }
 }
